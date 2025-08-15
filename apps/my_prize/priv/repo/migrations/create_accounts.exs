d1bf5eaa8e72510d38191a2a91f58490145353ff
Vinícius Bertoldi <bertoldi11@gmail.com>
2025-08-15 18:56:36 -0300

    Adicionando migration pras tabelas a serem utilizadas

---
+++ apps/my_prize/priv/repo/migrations/create_accounts.exs
@@ -0,0 +1,15 @@
+defmodule MyPrize.Repo.Migrations.CreateAccounts do
+  use Ecto.Migration
+
+  def change do
+    create table(:accounts, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string, null: false
+      add :email, :string, null: false
+
+      timestamps()
+    end
+
+    create unique_index(:accounts, [:email])
+  end
+end
---
+++ apps/my_prize/priv/repo/migrations/create_prizes.exs
@@ -0,0 +1,19 @@
+defmodule MyPrize.Repo.Migrations.CreatePrizes do
+  use Ecto.Migration
+
+  def change do
+    create table(:prizes, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string, null: false
+      add :description, :string, null: false
+      add :account_owner_id, :binary_id, null: false
+      add :expiration_date, :utc_date
+      add :raffle_winner_id, :binary_id
+      add :accounts_applied, {:array, :binary_id}, default: []
+
+      timestamps()
+    end
+
+    create unique_index(:prizes, [:name, :account_owner_id], name: :unique_prize_per_account)
+  end
+end
