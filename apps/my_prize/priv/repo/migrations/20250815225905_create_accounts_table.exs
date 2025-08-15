b3d3b0a9ef6191943e1bb43040b397a8fccda0ab
Vinícius Bertoldi <bertoldi11@gmail.com>
2025-08-15 19:57:00 -0300

    Ajustando nome do arquivo de migration

--- apps/my_prize/priv/repo/migrations/20250815225905_create_accounts_table.exs
+++ apps/my_prize/priv/repo/migrations/20250815225905_create_accounts_table.exs
@@ -2,6 +2,14 @@ defmodule MyPrize.Repo.Migrations.CreateAccountsTable do
   use Ecto.Migration
 
   def change do
+    create table(:accounts, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string, null: false
+      add :email, :string, null: false
 
+      timestamps()
+    end
+
+    create unique_index(:accounts, [:email])
   end
 end
--- apps/my_prize/priv/repo/migrations/20250815225921_create_prizes_table.exs
+++ apps/my_prize/priv/repo/migrations/20250815225921_create_prizes_table.exs
@@ -2,6 +2,18 @@ defmodule MyPrize.Repo.Migrations.CreatePrizesTable do
   use Ecto.Migration
 
   def change do
+    create table(:prizes, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string, null: false
+      add :description, :string, null: true
+      add :account_owner_id, :binary_id, null: false
+      add :expiration_date, :date
+      add :raffle_winner_id, :binary_id, null: true
+      add :accounts_applied, {:array, :binary_id}, default: []
 
+      timestamps()
+    end
+
+    create unique_index(:prizes, [:name, :account_owner_id], name: :unique_prize_per_account)
   end
 end
