f04ffef49e23134091bc67bae39bc4f02568fbe8
Vinícius Bertoldi <bertoldi11@gmail.com>
2025-08-15 19:20:18 -0300

    Apenas nome e data do sorteio sao obrigatorios

--- apps/my_prize/lib/my_prize/prizes/schemas/prizes.ex
+++ apps/my_prize/lib/my_prize/prizes/schemas/prizes.ex
@@ -10,7 +10,7 @@ defmodule MyPrize.Prizes.Schemas.Prizes do
     field :description, :string
     field :account_owner_id, :binary_id
     field :expiration_date, :date
-    field :raffle_winner_id, :binary_id, null: true
+    field :raffle_winner_id, :binary_id
     field :accounts_applied, {:array, :binary_id}, default: []
 
     timestamps()
@@ -19,8 +19,8 @@ defmodule MyPrize.Prizes.Schemas.Prizes do
   @doc false
   def changeset(prize, attrs) do
     prize
-    |> cast(attrs, [:name, :description, :raffle_at, :raffle_winner_id, :accounts_applied, :account_owner_id])
-    |> validate_required([:name, :description, :raffle_at])
+    |> cast(attrs, [:name, :description, :expiration_date, :raffle_winner_id, :accounts_applied, :account_owner_id])
+    |> validate_required([:name, :expiration_date])
     |> unique_constraint(:name, :account_owner_id, name: :unique_prize_per_account)
   end
 end
