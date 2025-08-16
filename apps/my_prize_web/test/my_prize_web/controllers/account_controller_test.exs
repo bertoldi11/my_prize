b79df8d8feb51515806a8ee8ebc941abdfc82fe3
Vinícius Bertoldi <bertoldi11@gmail.com>
2025-08-15 21:41:08 -0300

    Testes criação de contas

--- apps/my_prize_web/test/my_prize_web/controllers/account_controller_test.exs
+++ apps/my_prize_web/test/my_prize_web/controllers/account_controller_test.exs
@@ -11,12 +11,12 @@ defmodule MyPrizeWeb.Controllers.AccountControllerTest do
   describe "create account" do
     test "renders account when data is valid", %{conn: conn} do
       conn = post(conn, "/api/account", @create_attrs)
-      assert %{status: "success", account: _account} = json_response(conn, 200)
+      assert %{"status" => "success", "account" => _account} = json_response(conn, 200)
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
       conn = post(conn, "/api/account", @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
+      assert json_response(conn, 400)["errors"] != %{}
     end
   end
 end
